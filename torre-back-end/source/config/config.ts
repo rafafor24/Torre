20a314c63c5f629ecc9f1182b0b7d6829f750fdb
Rafael Forero Alvarado <ra.forero11@uniandes.edu.co>
2021-02-08 18:48:47 -0500

    Update config.ts

--- torre-back-end/source/config/config.ts
+++ torre-back-end/source/config/config.ts
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const SERVER_PORT = process.env.SERVER_PORT || 1337;
+const port = process.env.PORT || 1337;
 
 const SERVER = {
     hostname: SERVER_HOSTNAME,
-    port: SERVER_PORT
+    port: port
 };
 
 const config = {
